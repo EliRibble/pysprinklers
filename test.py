7211ca9cbdc5bb4f68849c5d2dfbf8b36cef2db4
Eli Ribble <junk@theribbles.org>
2012-06-23 21:49:52 -0600

    Now hav working iterator and able to set sprinklers manually

--- test.py
+++ test.py
@@ -2,10 +2,18 @@
 from Phidgets.PhidgetException import *
 from Phidgets.Events.Events import *
 from Phidgets.Devices.InterfaceKit import *
+import datetime
+import time
+import sys
 
 def attachHandler(args):
-	print(dir(args))
-	print(args.__class__.__name__)
+	#print(dir(args))
+	#print(args.__class__.__name__)
+	pass
+
+def print_now():
+	now = datetime.datetime.now()
+	print( now.strftime("%Y-%m-%d %H:%M") )
 
 def main():
 	try:
@@ -13,11 +21,26 @@ def main():
 		interface_kit.setOnAttachHandler(attachHandler)
 		interface_kit.openPhidget()
 		interface_kit.waitForAttach(1000)
-		print(interface_kit.getOutputCount())
+		if len(sys.argv) > 2:
+			input_num = int(sys.argv[1])
+			state = int(sys.argv[2]) == 1
+			interface_kit.setOutputState(input_num, state)
+		else:
+			print("Moving through sprinklers...")
+			for i in range(interface_kit.getOutputCount()):
+				print_now()
+				print( "Sprinkler {0} ON".format(i) )
+				interface_kit.setOutputState(i, True)
+				time.sleep( 60 * 40 )
+				print_now()
+				interface_kit.setOutputState(i, False)
+			
 	except RuntimeError as e:
 		print("Runtime error: %s" % e.message)
 	except PhidgetException as e:
 		print("Phidgetexception: {0}".format(e))
+	finally:
+		interface_kit.closePhidget()
 
 if __name__ == '__main__':
 	main()
