8ea6e30a308101dfb908e3bcf07944c79cfe9721
Eli Ribble <junk@theribbles.org>
2012-07-17 06:40:48 -0600

    Add some command options to mimic Dave's sprinkler controller
    I now have options like --all-off, which turns all valves off
    --run-zone <num> to run a specific zone...I should make a usage
    statement...

--- isprinkle-control.py
+++ isprinkle-control.py
@@ -15,32 +15,73 @@ def print_now():
 	now = datetime.datetime.now()
 	print( now.strftime("%Y-%m-%d %H:%M") )
 
+def all_off(interface_kit):
+	for i in range(interface_kit.getOutputCount()):
+		interface_kit.setOutputState(i, False)
+	print("All valves off")
+
+CURRENT_SCHEDULES = {
+	0: {
+		0: 45,
+		2: 45,
+		3: 45,
+	},
+	1: {
+		8:  40,
+		9:  40,
+		10: 45,
+		11: 45,
+		12: 45,
+	},
+}
+	
+SPRAY_TIME_MULTIPLIER = 60
+def run_schedule(schedule, interface_kit):
+	for valve, spray_time in schedule.items():
+		print("Valve {0} for {1}".format(valve, spray_time * SPRAY_TIME_MULTIPLIER))
+		interface_kit.setOutputState(valve, True)
+		time.sleep(spray_time * SPRAY_TIME_MULTIPLIER)
+		interface_kit.setOutputState(valve, False)
+	
+
 def main():
+	if len(sys.argv) < 2:
+		print("Please specify --all-off or --run-zone <number>")
+		sys.exit(1)
+	
+
+	interface_kit = InterfaceKit()
+	#interface_kit.setOnAttachHandler(attachHandler)
+	interface_kit.openPhidget()
 	try:
-		interface_kit = InterfaceKit()
-		interface_kit.setOnAttachHandler(attachHandler)
-		interface_kit.openPhidget()
 		interface_kit.waitForAttach(1000)
-		if len(sys.argv) > 2:
-			input_num = int(sys.argv[1])
-			state = int(sys.argv[2]) == 1
-			interface_kit.setOutputState(input_num, state)
-		else:
-			print("Moving through sprinklers...")
-			for i in range(interface_kit.getOutputCount()):
-				print_now()
-				print( "Sprinkler {0} ON".format(i) )
-				interface_kit.setOutputState(i, True)
-				time.sleep( 60 * 40 )
-				print_now()
-				interface_kit.setOutputState(i, False)
-			
+		if sys.argv[1] == '--all-off':
+			all_off(interface_kit)
+			sys.exit(0)
+		elif sys.argv[1] == '--run-zone':
+			if len(sys.argv) < 3:
+				print("Please spcify a zone number")
+				sys.exit(2)
+			else:
+				zone_number = int(sys.argv[2])
+				interface_kit.setOutputState(zone_number, True)
+				print("Valve {0} ON".format(zone_number))
+				if len(sys.argv) == 4:
+					min_to_run = int(sys.argv[3])
+					print("Running for {0} minutes".format(min_to_run))
+					time.sleep(min_to_run * 60)
+					interface_kit.setOutputState(zone_number, False)
+				sys.exit(0)
+		elif sys.argv[1] == '--run':
+			for section, schedule in CURRENT_SCHEDULES.items():
+				run_schedule(schedule, interface_kit)	
 	except RuntimeError as e:
 		print("Runtime error: %s" % e.message)
 	except PhidgetException as e:
 		print("Phidgetexception: {0}".format(e))
 	finally:
 		interface_kit.closePhidget()
+	sys.exit(3)
 
 if __name__ == '__main__':
 	main()
