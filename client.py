e9c2da490598cb8e8fce27d1891e55f398cd095a
Eli Ribble <junk@theribbles.org>
2013-06-27 13:03:50 -0600

    Add a simple client to test against the server

---
+++ client.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import dbus
+system_bus = dbus.SystemBus()
+
+proxy = system_bus.get_object('org.theribbles.HomeAutomation', '/sprinklers')
+iface = dbus.Interface(proxy, 'org.theribbles.HomeAutomation')
+print(iface.GetStatus())
