f1d15bf266048b205d382c6a065092e400f4550f
Eli Ribble <junk@theribbles.org>
2013-06-27 14:42:06 -0600

    Make the client script more flexible for testing

--- client.py
+++ client.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
+import sys
 import dbus
 system_bus = dbus.SystemBus()
 
 proxy = system_bus.get_object('org.theribbles.HomeAutomation', '/sprinklers')
 iface = dbus.Interface(proxy, 'org.theribbles.HomeAutomation')
-print(iface.GetStatus())
-print(iface.GetSprinklers())
+
+command = sys.argv[1]
+if command == 'status':
+    print(iface.GetStatus())
+elif command == 'sprinklers':
+    print(iface.GetSprinklers())
+elif command == 'on':
+    iface.SetSprinklerOn(sys.argv[2])
+elif command == 'off':
+    iface.SetSprinklerOff(sys.argv[2])
+elif command == 'onfor':
+    iface.SetSprinklerOnDuration(sys.argv[2], int(sys.argv[3]))
