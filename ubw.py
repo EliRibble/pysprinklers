2ef0440fa67286d12f72da446064bca16d1f5f3e
Eli Ribble <junk@theribbles.org>
2013-06-27 22:01:50 -0600

    Clean up ubw module
    This involves removing the old main, which we won't
    be using any more, and moving public methods together
    and private methods together

--- ubw.py
+++ ubw.py
@@ -26,6 +26,12 @@ class UBW(object):
     def __repr__(self):
         return "UBW on {0}".format(self.device_name)
 
+def _find_device():
+    for tty in os.listdir('/dev'):
+        if 'ACM' in tty:
+            return '/dev/' + tty
+    raise UBWException("Failed to find any devices like /dev/ACMx")
+
 def _get_device():
     device_name = _find_device()
     LOGGER.debug("Got device %s", device_name)
@@ -72,6 +78,21 @@ def _get_pin_states(ubw):
         'C': _to_bit_array(C)}
 _get_pin_states.pattern = re.compile('I,(?P<A>\d{3}),(?P<B>\d{3}),(?P<C>\d{3})')
 
+def _read_pin(ubw, port, pin):
+    response = _handle_command(ubw, 'PI,B,0')
+    match = _read_pin.pattern.match(response)
+    return bool(int(match.group('value')))
+_read_pin.pattern = re.compile('PI,(?P<value>[01])')
+
+def _get_pin(pin):
+    if pin in (0,1,2,3,4,5,6,7):
+        return pin
+    else:
+        for config in SETUP.values():
+            if pin in config:
+                return config[pin]
+        raise Exception('Unrecognized pin %s', pin)
+
 def get_status():
     ubw = _get_device()
     return _get_pin_states(ubw)
@@ -87,88 +108,3 @@ def set_pin(port, pin, state):
         'pin'   : pin,
         'state' : 1 if state else 0}))
 
-def read_pin(ubw, port, pin):
-    response = _handle_command(ubw, 'PI,B,0')
-    match = read_pin.pattern.match(response)
-    return bool(int(match.group('value')))
-read_pin.pattern = re.compile('PI,(?P<value>[01])')
-
-def _get_pin(pin):
-    if pin in (0,1,2,3,4,5,6,7):
-        return pin
-    else:
-        for config in SETUP.values():
-            if pin in config:
-                return config[pin]
-        raise Exception('Unrecognized pin %s', pin)
-    
-def _find_device():
-    for tty in os.listdir('/dev'):
-        if 'ACM' in tty:
-            return '/dev/' + tty
-    raise UBWException("Failed to find any devices like /dev/ACMx")
-
-def _is_finished(progress):
-    for config in SCHEDULE.values():
-        if _get_next(config, progress) is not None:
-            return False
-    return True
-
-def _get_next(config, progress):
-    for valve, time in config['valves'].items():
-        if valve not in progress:
-            progress[valve] = 0
-            return valve
-        elif progress[valve] < time:
-            return valve
-    return None
-
-def main():
-    device = _find_device()
-    if device is None:
-        print("Unable to find ACM device in /dev")
-        return 1
-    print("Using {0}".format(device))
-
-    ubw = init_ubw(device)
-
-    print(_handle_command(ubw, 'V'))
-    print("Set all port directions to output: {0}".format(_handle_command(ubw, 'C,0,0,0,0')))
-    front = SCHEDULE['front']
-    back = SCHEDULE['back']
-    progress = {}
-    chunk_size = min(10, DEFAULT_TIME)
-    try:
-        while not _is_finished(progress):
-            try:
-                current_front = _get_next(front, progress)
-                current_back = _get_next(back, progress)
-                print("Front: {0}\tBack: {1}".format(current_front, current_back))
-                all_off(ubw)
-                if current_front is not None:
-                    set_pin(ubw, SCHEDULE['front']['port'], current_front, True)
-                if current_back is not None:
-                    set_pin(ubw, SCHEDULE['back']['port'], current_back, True)
-                time.sleep(chunk_size * 60)
-                if current_front is not None:
-                    progress[current_front] += chunk_size
-                if current_back is not None:
-                    progress[current_back] += chunk_size
-            except (IOError, serial.serialutil.SerialException), e:
-                traceback.print_exc()
-                print("Progess before failure:")
-                pprint.pprint(progress)
-                time.sleep(5)
-                device = _find_device()
-                print("Now using {0}".format(device))
-                if device is None:
-                    raise Exception("Cannot get a device anymore")
-                ubw = init_ubw(device)
-                print(_handle_command(ubw, 'V'))
-                print("Set all port directions to output: {0}".format(_handle_command(ubw, 'C,0,0,0,0')))
-                all_off(ubw)
-    finally:
-        print('ALL OFF:{0}'.format(all_off(ubw)))
-
-if __name__ == '__main__':
-    sys.exit(main())
