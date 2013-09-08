c88341e9f104ad9c8f96bb9455e5f110a5700945
Eli Ribble <junk@theribbles.org>
2013-09-08 14:27:51 -0600

    Make the sprinlers endpoint work
    Also start blocking out an endpoint for individual sprinklers data

--- pysprinklers/web_server.py
+++ pysprinklers/web_server.py
@@ -12,7 +12,11 @@ def root():
 @server.route('/sprinklers/')
 def sprinklers():
     sprinklers = platform.get_sprinklers()
-    return json.dumps(data, sort_keys=True), 200, {'Content-Type': 'text/json'}
-    
+    return json.dumps(sprinklers, sort_keys=True), 200, {'Content-Type': 'text/json'}
+ 
+@server.route('/sprinklers/<sprinkler_id>/')   
+def sprinkler(sprinkler_id):
+    return "Hey {0}".format(sprinkler_id)
+
 def run(config):
     server.run()
